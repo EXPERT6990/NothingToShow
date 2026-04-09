60d7bc938e4bcac9be7bfd3e2b6daccfac1c2326
EXPERT6990 <amiexpert69@gmail.com>
2026-04-09 10:00:33 +0530

    modified:   auto.py

--- auto.py
+++ auto.py
@@ -10,34 +10,34 @@ file_path = "./AutoCommitHistory.json"
 last_commit = repo.head.commit
 last_date = datetime.fromtimestamp(last_commit.committed_date).date()
 today = datetime.now().date()
+# Load or initialize JSON
+if os.path.exists(file_path):
+    with open(file_path, "r") as f:
+        data = json.load(f)
+else:
+    data = {"count": 0, "history": []}
 
-if last_date < today:
-    print("No commit today. Updating JSON...")
+# if last_date < today:
+if True:
+    # print("No commit today. Updating JSON...")
+    while(True):
+        # Prevent duplicate entry
+        if str(today) not in data["history"]:
+            data["count"] += 1
+            data["history"].append(str(today))
 
-    # Load or initialize JSON
-    if os.path.exists(file_path):
-        with open(file_path, "r") as f:
-            data = json.load(f)
-    else:
-        data = {"count": 0, "history": []}
+            with open(file_path, "w") as f:
+                json.dump(data, f, indent=4)
 
-    # Prevent duplicate entry
-    if str(today) not in data["history"]:
-        data["count"] += 1
-        data["history"].append(str(today))
+            # Commit changes
+            repo.git.add(file_path)
+            repo.index.commit(f"Auto commit: inactivity logged for {today}")
 
-        with open(file_path, "w") as f:
-            json.dump(data, f, indent=4)
-
-        # Commit changes
-        repo.git.add(file_path)
-        repo.index.commit(f"Auto commit: inactivity logged for {today}")
-
-        try:
-            origin = repo.remote(name="origin")
-            origin.push()
-            print("Auto commit pushed.")
-        except Exception as e:
-            print("Push failed:", e)
+            try:
+                origin = repo.remote(name="origin")
+                origin.push()
+                print("Auto commit pushed.")
+            except Exception as e:
+                print("Push failed:", e)
 else:
     print("Commit already exists today. No action needed.")
