006fc02d8966a639197147cb11be86f55a42e05b
EXPERT6990 <amiexpert69@gmail.com>
2026-04-09 20:56:39 +0530

    ggg

--- main.cpp
+++ main.cpp
@@ -4,17 +4,26 @@
 #include <chrono>
 #include <thread>
 using namespace std;
+
+void thread_func() {
+    while(true) {
+        cout << "\033[2J\033[1;1H"; // Clear the console
+        system("git push origin main");
+        this_thread::sleep_for(chrono::seconds(1));
+    }
+}
+
 int main() {
-   for(int i = 0; ; i++) {
+    thread t(thread_func);
+    t.detach();
+    for(int i = 0; ; i++) {
     FILE *fp = fopen("file.txt", "w");
     fprintf(fp, "%d", i);
     fclose(fp);
     system("git add file.txt");
     string cmd = "git commit -m \"commit " + to_string(i) + "\"";
-    system(cmd.c_str());
-    cmd = "git push origin main";
-    system(cmd.c_str());
-    this_thread::sleep_for(chrono::seconds(3));
+    system(cmd.c_str());    
    }
+   t.join();
     return 0;
 }
